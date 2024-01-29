9fb9521ae4458b6300260f97ec4e815b3a95a916
Marco Ammann <46301477+Melendros@users.noreply.github.com>
2024-01-29 16:20:47 +0100

    Update endboss.class.js

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -4,9 +4,11 @@ class Endboss extends MovableObject {
    x = 5800;
    y = -100;
    collisionBoxWidth = this.width * 0.75;
-   collisionBoxHeight = this.height * 0.30; 
+   collisionBoxHeight = this.height * 0.3;
    collisionBoxOffsetX = 50;
    collisionBoxOffsetY = 295;
+
+   isAttacking = false;
 
    IMAGES_SWIM = [
       'img/2.Enemy/3.Final_Enemy/2.floating/1.png',
@@ -24,15 +26,36 @@ class Endboss extends MovableObject {
       'img/2.Enemy/3.Final_Enemy/2.floating/13.png',
    ];
 
+   IMAGES_ATTACK = [
+      'img/2.Enemy/3.Final_Enemy/Attack/1.png',
+      'img/2.Enemy/3.Final_Enemy/Attack/2.png',
+      'img/2.Enemy/3.Final_Enemy/Attack/3.png',
+      'img/2.Enemy/3.Final_Enemy/Attack/4.png',
+      'img/2.Enemy/3.Final_Enemy/Attack/5.png',
+      'img/2.Enemy/3.Final_Enemy/Attack/6.png',
+   ];
+
    constructor() {
       super().loadImage('img/2.Enemy/3.Final_Enemy/2.floating/1.png');
       this.loadImages(this.IMAGES_SWIM);
+      this.loadImages(this.IMAGES_ATTACK);
       this.animate();
    }
 
    animate() {
+
+      
       this.animationInterval = setInterval(() => {
          this.playAnimation(this.IMAGES_SWIM);
       }, 1000 / 8);
+
+      this.attackIntervall = setInterval(() => {
+         this.isAttacking = true;
+         this.playAnimation(this.IMAGES_ATTACK);
+         setTimeout(() => {
+            this.isAttacking = false;
+         }, 300);
+      }, 1500);
+      
    }
 }
