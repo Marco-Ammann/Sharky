a09f6b6c6608dee8a6a4e5c9a1153eeac2b0daf6
Marco Ammann <46301477+Melendros@users.noreply.github.com>
2024-02-08 23:44:36 +0100

    added JSDocs to game.js

--- js/game.js
+++ js/game.js
@@ -13,18 +13,27 @@ let animationFrameId = [];
 
 let gameRestartet = false;
 
+/**
+ * Clears all timeouts stored in globalTimeouts array.
+ */
 function clearAllTimeOuts() {
    globalTimeouts.forEach((id) => clearTimeout(id));
    globalTimeouts = [];
 }
 
 
+/**
+ * Clears all intervals stored in globalIntervals array.
+ */
 function clearAllIntervals() {
    globalIntervals.forEach((id) => clearInterval(id));
    globalIntervals = [];
 }
 
 
+/**
+ * Cancels the animation frame request.
+ */
 function clearAnimationFrame() {
    if (animationFrameId) {
       cancelAnimationFrame(animationFrameId);
@@ -33,6 +42,9 @@ function clearAnimationFrame() {
 }
 
 
+/**
+ * Initializes the game, sets up the world, and starts the game logic.
+ */
 function startGame() {
    document.getElementById('main-menu').style.display = 'none';
    document.getElementById('canvas').style.display = 'block';
@@ -41,7 +53,6 @@ function startGame() {
    document.getElementById('title').style.display = 'none';
    if (window.innerHeight < 800) {
       document.querySelector('.mobilePanel').style.display = 'flex';
-      
    }
    world.startMusic();
    checkIfCharacterOrBossIsDead();
@@ -55,6 +66,9 @@ function startGame() {
 }
 
 
+/**
+ * Checks whether the character or the endboss is dead and updates the game state accordingly.
+ */
 function checkIfCharacterOrBossIsDead() {
    let checkInterval = setInterval(() => {
       if (world.character.healthPoints <= 0 && checkIntervalOn) {
@@ -63,7 +77,7 @@ function checkIfCharacterOrBossIsDead() {
          world.stopMusic();
          setTimeout(() => {
             checkIntervalOn = false;
-            
+
             setTimeout(() => {
                document.getElementById('gameOverScreen').style.display = 'flex';
                clearAll();
@@ -91,6 +105,9 @@ function checkIfCharacterOrBossIsDead() {
 }
 
 
+/**
+ * Initializes the game environment and preloads necessary resources.
+ */
 function init() {
    canvas = document.getElementById('canvas');
    keyboard = new Keyboard();
@@ -105,69 +122,81 @@ function init() {
 }
 
 
+/**
+ * Creates a new game World instance
+ */
 function createNewWorld() {
    world = new World(canvas, keyboard, createLevel1());
 }
 
 
+/**
+ * toggles the games sound between muted and unmuted
+ */
 function switchSoundMute() {
    let button = document.getElementById('switchSoundMuteButton');
-   button.classList.toggle('muted')
+   button.classList.toggle('muted');
    musicIsPlaying = !musicIsPlaying;
    musicIsPlaying ? world.startMusic() : world.stopMusic();
 }
 
 
-
-
-
+/**
+ * toggles the game's play state between paused and resumed
+ * @return cancel pause request while Boss is attacking
+ */
 function switchPlayPause() {
-   const endboss = world.level.enemies.find(enemy => enemy instanceof Endboss);
+   const endboss = world.level.enemies.find((enemy) => enemy instanceof Endboss);
    if (endboss.isAttacking) {
-       console.log('cant pause during boss attack');
-       return;
+      console.log('cant pause during boss attack');
+      return;
    }
    gameIsPaused = !gameIsPaused;
 
    const playButton = document.getElementById('playButton');
    if (gameIsPaused) {
-       pauseGame();
-       playButton.textContent = 'resume';
-       playButton.classList.add('paused');
+      pauseGame();
+      playButton.textContent = 'resume';
+      playButton.classList.add('paused');
    } else {
-       resumeGame();
-       playButton.textContent = 'pause';
-       playButton.classList.remove('paused');
+      resumeGame();
+      playButton.textContent = 'pause';
+      playButton.classList.remove('paused');
    }
 }
 
 
+/**
+ * pauses the game, stopping animations and gameplay
+ */
 function pauseGame() {
    world.isGamePaused = true;
-   const endboss = world.level.enemies.find(enemy => enemy instanceof Endboss);
+   const endboss = world.level.enemies.find((enemy) => enemy instanceof Endboss);
    if (!endboss.isAttacking) {
-      
       console.log('Game paused');
       switchSoundMute();
       world.character.clearIntervals();
       world.level.enemies.forEach((enemy) => {
+         enemy.clearIntervals();
+         if (enemy instanceof Endboss) {
             enemy.clearIntervals();
-            if (enemy instanceof Endboss) {
-               enemy.clearIntervals();
-            }
+         }
       });
-   
+
       world.level.collectables.forEach((collectable) => {
          collectable.clearIntervals();
       });
-   
+
       cancelAnimationFrame(animationFrameId);
    } else {
-      console.log('cant pause during boss attack')
+      console.log('cant pause during boss attack');
    }
 }
 
 
+/**
+ * resumes the game from a paused state, restarting animations and gameplay
+ */
 function resumeGame() {
    world.isGamePaused = false;
    clearAll();
@@ -178,7 +207,7 @@ function resumeGame() {
    world.level.enemies.forEach((enemy) => {
       if (!(enemy instanceof Endboss)) {
          enemy.animate();
-      } else if (enemy instanceof Endboss && enemy.isIntroduced){
+      } else if (enemy instanceof Endboss && enemy.isIntroduced) {
          enemy.animate();
       }
    });
@@ -190,6 +219,9 @@ function resumeGame() {
 }
 
 
+/**
+ * Resets the game to its initial state.
+ */
 function resetGame() {
    clearAll();
    document.getElementById('gameOverScreen').style.display = 'none';
@@ -204,7 +236,7 @@ function resetGame() {
    }
 
    world.stopMusic();
-   
+
    setTimeout(() => {
       gameRestartet = true;
       checkIntervalOn = true;
@@ -215,6 +247,9 @@ function resetGame() {
 }
 
 
+/**
+ * Clears all timeouts, intervals, and animation frames to stop all ongoing game actions.
+ */
 function clearAll() {
    clearAllTimeOuts();
    clearAllIntervals();
@@ -222,22 +257,40 @@ function clearAll() {
 }
 
 
-
+/**
+ * Simulates a keydown event for a given key code.
+ * @param {string} keyCode - The code of the key to simulate.
+ */
 function simulateKeyDown(keyCode) {
    const event = new KeyboardEvent('keydown', { code: keyCode });
    window.dispatchEvent(event);
 }
 
+
+/**
+ * Simulates a keyup event for a given key code.
+ * @param {string} keyCode - The code of the key to simulate.
+ */
 function simulateKeyUp(keyCode) {
    const event = new KeyboardEvent('keyup', { code: keyCode });
    window.dispatchEvent(event);
 }
 
+
+/**
+ * Handles the start of a touch interaction, simulating a keydown event.
+ * @param {string} keyCode - The code of the key to simulate.
+ */
 function handleTouchStart(keyCode) {
    simulateKeyDown(keyCode);
    event.preventDefault();
 }
 
+
+/**
+ * Handles the end of a touch interaction, simulating a keyup event.
+ * @param {string} keyCode - The code of the key to simulate.
+ */
 function handleTouchEnd(keyCode) {
    simulateKeyUp(keyCode);
    event.preventDefault();
